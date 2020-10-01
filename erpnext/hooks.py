efcc0357631fb758895b46de3556bdd27a65aac4
abhimanyu888 <43754033+abhimanyu888@users.noreply.github.com>
2020-10-01 13:24:56 +0530

    Update hooks.py

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -35,12 +35,12 @@ app_license = "MIT"
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+ home_page = "login"
 
-# website user home page (by Role)
-# role_home_page = {
-#	"Role": "home_page"
-# }
+ website user home page (by Role)
+ role_home_page = {
+	"Role": "home_page"
+ }
 
 # Website user home page (by function)
 # get_website_user_home_page = "erpnext.utils.get_home_page"
